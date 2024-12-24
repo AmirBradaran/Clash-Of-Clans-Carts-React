087c13e99e56a16d70de1b0a0695a61ef8c830cb
Amir <baradaran13085@gmail.com>
2024-12-24 14:04:06 +0330

    Set API With Cart.jsx
    create a Units.jsx for set units for each carts

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,39 +1,24 @@
 import React from "react";
+import Units from "./Units";
 
 export default function Cart({ hero }) {
+  const { name, level, img, units, description } = hero;
+  const unites = units?.map((e, index) => (
+    <Units key={index} state={e.state} value={e.value} />
+  ));
   return (
-    <div class="clash-card barbarian">
-      <div class="clash-card__image clash-card__image--barbarian">
-        <img
-          src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/195612/barbarian.png"
-          alt="barbarian"
-        />
+    <div class={`clash-card ${name}`}>
+      <div class={`clash-card__image clash-card__image--${name}`}>
+        <img src={img} alt={`${name}`} />
       </div>
-      <div class="clash-card__level clash-card__level--barbarian">Level 4</div>
-      <div class="clash-card__unit-name">The Barbarian</div>
-      <div class="clash-card__unit-description">
-        The Barbarian is a kilt-clad Scottish warrior with an angry,
-        battle-ready expression, hungry for destruction. He has Killer yellow
-        horseshoe mustache.
-      </div>
+      <div class={`clash-card__level clash-card__level--${name}`}>{level}</div>
+      <div class={`clash-card__unit-name`}>The {name}</div>
+      <div class={`clash-card__unit-description`}>{description}</div>
 
-      <div class="clash-card__unit-stats clash-card__unit-stats--barbarian clearfix">
-        <div class="one-third">
-          <div class="stat">
-            20<sup>S</sup>
-          </div>
-          <div class="stat-value">Training</div>
-        </div>
-
-        <div class="one-third">
-          <div class="stat">16</div>
-          <div class="stat-value">Speed</div>
-        </div>
-
-        <div class="one-third no-border">
-          <div class="stat">150</div>
-          <div class="stat-value">Cost</div>
-        </div>
+      <div
+        class={`clash-card__unit-stats clash-card__unit-stats--${name} clearfix`}
+      >
+        {unites}
       </div>
     </div>
   );
--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -2,37 +2,43 @@ import React, { useEffect, useState } from "react";
 import Cart from "./Cart";
 
 export default function Slider() {
-    const [heroes , setHeroes]=useState(0)
-    const [currentIndex,setCurrentIndex]=useState()
-    useEffect(()=>{
-        (async()=>{
-            try {
-                const res=await fetch(`http://localhost:3000/heroes`)
-                const data=await res.json()
-                setHeroes()
-            } catch (error) {
-                console.log(Error - Fetch)
-            }
-        })()
-    },[])
+  const [heroes, setHeroes] = useState();
+  const [currentIndex, setCurrentIndex] = useState(0);
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await fetch(`http://localhost:3000/heroes`);
+        const data = await res.json();
+        setHeroes(data);
+      } catch (error) {
+        console.log(Error - Fetch);
+      }
+    })();
+  }, []);
   return (
     <div class="slide-container">
       <div class="wrapper">
-        <div class="arrow-wrapper">
-          <div class="round">
-            <div id="cta">
-              <span class="arrow prev"></span>
+        {heroes ? (
+          <>
+            <div class="arrow-wrapper">
+              <div class="round">
+                <div id="cta">
+                  <span class="arrow prev"></span>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-        <Cart hero={heroes[currentIndex]} />
-        <div class="arrow-wrapper">
-          <div class="round">
-            <div id="cta">
-              <span class="arrow next"></span>
+            <Cart hero={heroes[currentIndex]} />
+            <div class="arrow-wrapper">
+              <div class="round">
+                <div id="cta">
+                  <span class="arrow next"></span>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
+          </>
+        ) : (
+          <div className="loader"> Loading ... </div>
+        )}
       </div>
     </div>
   );
